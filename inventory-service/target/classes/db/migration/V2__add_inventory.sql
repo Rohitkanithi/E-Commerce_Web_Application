43cb891eef032833d83f7479f349cf27ea23a9f2
Rohitkanithi <63586900+Rohitkanithi@users.noreply.github.com>
2025-02-27 16:20:01 -0600

    files updated
    files updated

---
+++ inventory-service/target/classes/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory (sku_code, quantity, price)
+values ('iphone_16', 1500, 800),
+    ('iphone_16s', 2000, 1000),
+    ('iphone_16pro', 5000, 1200),
+    ('iphone_16proMax', 8000, 1400);
